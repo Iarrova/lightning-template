9534e9dd439fa2ca2cdafe189b3213cb988c9357
Ian Roberts <ian.roberts.valenzuela@gmail.com>
2024-08-25 22:43:01 -0400

    Add testing metrics and confusion matrix

--- test.py
+++ test.py
@@ -1,24 +1,34 @@
-import argparse
+from argparse import ArgumentParser, Namespace
+import seaborn as sns
+import matplotlib.pyplot as plt
 
 import torch
 import torch.nn as nn
 
 import lightning as L
 
-from torchmetrics.classification import Accuracy
+from torchmetrics import MetricCollection
+from torchmetrics.classification import (
+    Accuracy,
+    Precision,
+    Recall,
+    AUROC,
+    ConfusionMatrix
+)
 
+from utils.config import Config
 from utils.json_parser import parse_json
 
-parser = argparse.ArgumentParser(
+parser: ArgumentParser = ArgumentParser(
     description="Template for PyTorch Lightning prototyping."
 )
 parser.add_argument(
     "--config_path",
     help="The path to the configuration file to use in training/testing.",
 )
-args = parser.parse_args()
+args: Namespace = parser.parse_args()
 
-config = parse_json(args.config_path)
+config: Config = parse_json(args.config_path)
 
 L.seed_everything(config.seed)
 if torch.cuda.is_available():
@@ -27,7 +37,6 @@ else:
     print("[INFO] CUDA is not available. Training on CPU...")
 
 
-# Dataset
 if config.dataset == "CIFAR10":
     from datasets.CIFAR10 import generate_CIFAR10 as generate_dataset
 
@@ -43,23 +52,32 @@ train_loader, validation_loader, test_loader, classes = generate_dataset(
 )
 
 
-# Network
 if config.network == "ResNet50":
     from networks.resnet50 import ResNet50 as network
 else:
     print("[ERROR] Currently only ResNet50 network is supported. Exiting...")
 
-model = network(include_top=False, weights="imagenet", num_classes=num_classes)
+model: network = network(
+    include_top=config.include_top, weights=config.weights, num_classes=num_classes
+)
 
 
 # PyTorch Lightning
-class LitModel(L.LightningModule):
-    def __init__(self, model, criterion):
+class Model(L.LightningModule):
+    def __init__(self, model: network, criterion):
         super().__init__()
-        self.model = model
+        self.model: network = model
         self.criterion = criterion
 
-        self.test_accuracy = Accuracy(task="multiclass", num_classes=len(classes))
+        self.metrics = MetricCollection(
+            {
+                "accuracy": Accuracy(task="multiclass", num_classes=num_classes),
+                "precision": Precision(task="multiclass", num_classes=num_classes, average="macro"),
+                "recall": Recall(task="multiclass", num_classes=num_classes, average="macro"),
+                "auc": AUROC(task="multiclass", num_classes=num_classes),
+            }
+        )
+        self.confusion_matrix = ConfusionMatrix(task="multiclass", num_classes=num_classes)
 
     def step(self, batch):
         inputs, target = batch
@@ -69,23 +87,28 @@ class LitModel(L.LightningModule):
 
     def test_step(self, batch, batch_idx):
         loss, output, target = self.step(batch)
-        self.test_accuracy.update(output, target)
-        self.log(
-            "test_loss", loss, on_step=False, on_epoch=True, prog_bar=True, logger=True
-        )
-        self.log(
-            "test_acc",
-            self.test_accuracy,
-            on_step=False,
-            on_epoch=True,
-            prog_bar=True,
-            logger=True,
-        )
+        self.metrics.update(output, target)
+        self.confusion_matrix.update(output, target)
 
 
 criterion = nn.CrossEntropyLoss()
 weights_path = f"{config.weights_dir}/{config.weights_path}.ckpt"
-litmodel = LitModel.load_from_checkpoint(weights_path, model=model, criterion=criterion)
+model = Model.load_from_checkpoint(weights_path, model=model, criterion=criterion)
 
 trainer = L.Trainer()
-trainer.test(model=litmodel, dataloaders=test_loader)
+trainer.test(model=model, dataloaders=test_loader)
+
+for metric_name, metric_instance in model.metrics.items():
+    print(f"{metric_name}: {metric_instance.compute()}")
+
+fig, ax = plt.subplots(figsize=(8, 6))
+class_names = list(classes.keys())
+sns.heatmap(model.confusion_matrix.compute(), annot=True, fmt='d', cmap='Blues', xticklabels=class_names, yticklabels=class_names, ax=ax)
+ax.tick_params(axis='x', labelrotation=45)
+ax.set_xlabel("Predicted Labels")
+ax.set_ylabel("True Labels")
+ax.set_title("Confusion Matrix")
+
+# Save the confusion matrix as an image
+plt.savefig("confusion_matrix.png")
+plt.close(fig)
